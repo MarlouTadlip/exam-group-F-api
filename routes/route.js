ef9db70933bc3473bbf8253214f440bba4636ae0
unknown <YY343@ccs.local>
2025-03-27 21:24:14 +0800

    feat: add POST /exams endpoint

--- routes/route.js
+++ routes/route.js
@@ -30,11 +30,10 @@ router.put('/:id', (req, res) => {
 });
 
 
-rrouter.post('/', (req, res) => {
+router.post('/exams', (req, res) => {
   const newExam = req.body;
-  newExam.id = exams.length + 1; // Simple ID assignment
+  newExam.id = exams.length + 1; 
   exams.push(newExam);
   res.status(201).json(newExam);
 });
-
 module.exports = router;
