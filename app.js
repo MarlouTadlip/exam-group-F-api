4a6a6dc22ff972a5f5900f0ad393db8e88069687
unknown <YY343@ccs.local>
2025-03-27 20:30:53 +0800

    'changes'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import router from './routes/route.js'
 const app = express()
 
 app.use('/', router)
+//COMMENT FOR CONFLICT
 app.listen(3000, () => {
   console.log('Server running on port : 3000')
 })
