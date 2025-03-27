ddc40848ccf175191cfd81c277aea1b098300414
unknown <YY343@ccs.local>
2025-03-27 21:10:34 +0800

    feat: add PUT /:id endpoint

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import express from 'express'
 import {router }from './routes/route.js'
+
 const app = express()
+
+
+app.use(bodyParser.json()); 
+let exams = [
+  { id: 1, name: 'Math Exam', date: '2025-03-25' },
+  { id: 2, name: 'Science Exam', date: '2025-03-26' }
+];
 
 app.use('/', router)
 //COMMENT FOR CONFLICT
